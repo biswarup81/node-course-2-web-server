cb01fd5090d8409d7d0b9c0c2c91acd4b6bafc4a
Biswarup <biswarup.ghoshal@gmail.com>
2018-11-10 23:30:20 +0100

    added one arrow fuction in append file

--- server.js
+++ server.js
@@ -14,7 +14,11 @@ app.use((req, res, next) => {
   var log = `${now}: ${req.method} ${req.url}`;
 
   console.log(log);
-  fs.appendFile('server.log', log + '\n');
+  fs.appendFile('server.log', log + '\n', (err)=>{
+      if(err){
+        console.log("Error appending file");
+      }
+  });
   next();
 });
 
