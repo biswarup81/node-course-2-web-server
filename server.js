31e648c64fd5fe2490afb3002c45d2ffedd32d1d
Biswarup <biswarup.ghoshal@gmail.com>
2018-11-10 12:12:43 +0100

    Initial Commit

---
+++ server.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const hbs = require('hbs');
+
+
+var app = express();
+
+hbs.registerPartials(__dirname+'/views/partials');
+app.set('view engine','hbs');
+app.use(express.static(__dirname+'/public'));
+
+app.use((req,res,next) =>{
+    var now = new Date().toString();
+
+    console.log(`${now}`);
+    next();
+});
+
+hbs.registerHelper('getCurrentYear', () =>{
+    return new Date().getFullYear();
+});
+
+hbs.registerHelper('screamIt',(text) =>{
+    return text.toUpperCase();
+})
+app.get('/',(req, res)=>{
+    res.render('home.hbs',{
+        pageTitle:'Home page',
+        welcomeMessage: 'Awesome Learning.'
+        
+    })
+});
+
+app.get('/about',(req,res)=>{
+    res.render('about.hbs',{
+        pageTitle: 'About Page'
+    });
+})
+
+app.get('/bad',(req,res) =>{
+    res.send({
+        errorMessage: 'Unable to handle the Request'
+    })
+})
+
+app.listen(3000);
